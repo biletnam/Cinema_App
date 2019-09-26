be78ae733a7848fd0584861000d5e46e8495b47b
Celina <celinawierzbicka@gmail.com>
2019-09-26 22:58:54 +0200

    movie model expanded

--- server/src/models/movie.js
+++ server/src/models/movie.js
@@ -2,15 +2,34 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const movieSchema = new mongoose.Schema({
-    title: String,
-    
+    title: {
+        type: String,
+        reqired: true
+    },    
+    description: {
+        type: String,
+        reqired: true,
+        minlength: 5,
+        maxlength: 300
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    }
 });
 
 const Movie = mongoose.model('Movie', movieSchema, 'movies');
 
 function validateMovie(movie) {
     const schema = {
-        title: Joi.string().required()
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        director: Joi.string().required(),
+        year: Joi.number().required()
     }
     return Joi.validate(movie, schema);
 }

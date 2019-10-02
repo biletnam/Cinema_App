649f25440405200e96f34c04c8d4c8ee44a663cd
Bohdan <bohdanimiela@gmail.com>
2019-10-02 17:37:32 +0200

    Fix bug in generator

--- server/src/controllers/generateShows.js
+++ server/src/controllers/generateShows.js
@@ -1,41 +1,43 @@
-// const { Show, validate } = require('../models/show');
+const { Show, validate } = require('../models/show');
 const { Movie } = require('../models/movie');
 
 const generateShows = async (req, res) => {
   try {
     const day = Number(req.params.id);
     const movies = await Movie.find().sort('-popularity');
-    const startHoursOfShows = ['10', '13', '16', '19', '22'];
+    const startHoursOfShows = ['10', '13', '16', '22'];
     const primeTimeHour = '19';
     const price = 20;
     const premiumPrice = Math.round(price * 1.5);
     const seats = generateSeats();
-       // weekdays counting from monday at 0, so weekend is saturday = 5 and sunday = 6
+    // weekdays counting from monday at 0, so weekend is saturday = 5 and sunday = 6
     const saturday = 5;
     const isWeekend = day >= saturday;
 
     let shows = [];
 
     shows = startHoursOfShows.map((startHour, i)=>{
-      const isPremiumHour = primeTimeHour.includes(startHour);
-      let movie = null;
-      if (isPremiumHour){
-        movie = movies[isWeekend ? 0 : day]._id;
-      } else {
-        movie = movies[isWeekend ? i : day + (i*5)]._id;
-      }
-      return {
-        movie: movie,
+      const show = {
+        movie: movies[isWeekend ? i : day + (i*5)]._id,
         day: day,
         hour: startHour,
-        price: (isWeekend || isPremiumHour) ? premiumPrice : price,
+        price: isWeekend ? premiumPrice : price,
         seatsAvailable: seats
       }
-    })
-    
+      return show;
+    });
+    const primeTimeShow = {
+        movie: movies[isWeekend ? 0 : day]._id,
+        day: day,
+        hour: primeTimeHour,
+        price: premiumPrice,
+        seatsAvailable: seats
+    }
+    shows.push(primeTimeShow);
     res.send(shows);
+
   } catch (error) {
-      res.status(500).send('An error occured.');
+      res.status(500).send('An error occured here?.');
   }
 }
 

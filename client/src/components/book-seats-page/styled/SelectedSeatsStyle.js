1e03b09ad7b450071589be723c0ae2a55343f771
Bohdan <bohdanimiela@gmail.com>
2019-10-04 16:44:13 +0200

    Minor style adjustments

--- client/src/components/book-seats-page/styled/SelectedSeatsStyle.js
+++ client/src/components/book-seats-page/styled/SelectedSeatsStyle.js
@@ -16,7 +16,7 @@ export const Prices = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
-  margin: 5px auto;
+  margin: 10px auto 5px;
   max-width: 700px;
   padding: 10px;
 `;
@@ -26,9 +26,10 @@ export const ProceedBtn = styled.button`
   border: none;
   border-radius: 10px;
   color: ${props => props.theme.colors.main};
+  font: inherit;
   margin: 20px auto;
   outline: none;
-  padding: 5px 10px;
+  padding: 10px 20px;
   transition: color 0.5s;
   &:hover {
     color: ${props => props.theme.colors.secondary};

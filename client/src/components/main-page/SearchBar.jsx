abcf77e01ab7b911e57d5319c70d473ce24e078e
Aleksander Grzelak <alek67890@users.noreply.github.com>
2019-10-05 15:13:27 +0200

    add functionality to search Button

--- client/src/components/main-page/SearchBar.jsx
+++ client/src/components/main-page/SearchBar.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { selectMovie } from '../../store/actions';
+import movies from '../../services/movies';
+
 import { Bar, SearchInput, SearchIcon } from './styled/SearchBarStyle';
 
-const SearchBar = props => {
-  return (
-    <Bar>
-      <SearchInput placeholder={props.text} />
-      <SearchIcon className="fas fa-search"/>
-    </Bar>
-  );
+class SearchBar extends React.Component{
+  state = {term: ''}
+
+  keyPressed = (event) => {
+    if (event.key === "Enter") {
+      this.searchAndUpdate()
+    }
+  }
+
+  async searchAndUpdate(){
+    const response = await movies.get('/');
+    let regexp = new RegExp(`.*${this.state.term}.*`, 'i');
+    let matches = response.data.filter((task) => task.title.match(regexp));
+
+    if (matches.length>0){
+      this.props.selectMovie(matches[0]);
+    }
+  }
+
+  render(){
+    return (
+      <Bar>
+        <SearchInput
+          type="text"
+          onChange={(e)=>this.setState({term : e.target.value})}
+          onKeyPress={this.keyPressed}
+          value={this.state.term}
+          placeholder={this.props.text}
+        />
+        <SearchIcon className="fas fa-search" onClick={()=>this.searchAndUpdate()}/>
+      </Bar>
+    )
+  }
 }
 
-export default SearchBar;
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(
+mapStateToProps,
+{ selectMovie }
+)(SearchBar);
